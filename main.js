3c16022478b283b6c89e983ded5363bfe5fbf139
Sindhu-Vasireddy <98752594+Sindhu-Vasireddy@users.noreply.github.com>
2022-05-15 23:26:59 +0100

    Added Usernames and Image

--- main.js
+++ main.js
@@ -22,7 +22,9 @@ async function Login(Issuer) {
     }
     //OIDC error message 
     else {
-    document.getElementById('output').textContent="OIDC Issuer not found or not supported by this app.";
+    document.getElementById('output').textContent="OIDC Issuer not found in webid or invalid webID. Try logging in with either solidcommunity username or directly with CSS oidc Issuer";
+    document.getElementById('Issuer_SC').classList.add('hidden');
+    document.getElementById('Issuer_CSS').classList.add('hidden');
   }
 }
 //-------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
@@ -30,16 +32,18 @@ async function toHide(){
             document.getElementById('message').classList.add('hidden');
             document.getElementById('Issuer_SC').classList.add('hidden');
             document.getElementById('Issuer_CSS').classList.add('hidden');
+            document.getElementById('webid').classList.add('hidden');
+            document.getElementById('webid_Login').classList.add('hidden');
+
 }
 //-------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
 async function stepstoExecute(){
 
             document.getElementById('start_posting').classList.remove('hidden');
-            document.getElementById('start_posting').addEventListener('click', async () => { GetCoordinates();document.getElementById('submit').classList.remove('hidden');
-              document.getElementById('map').classList.remove('hidden');document.getElementById('submit').classList.remove('hidden');
-              document.getElementById('latlong').classList.remove('hidden');document.getElementById('start_posting').classList.add('hidden') 
-              document.getElementById('stop').classList.remove('hidden');await getParticipants();fetchLocations();//document.getElementById('selectPerson').classList.remove('hidden');
+            document.getElementById('start_posting').addEventListener('click', async () => { GetCoordinates();
+            document.getElementById('map').classList.remove('hidden');document.getElementById('start_posting').classList.add('hidden'); 
+            document.getElementById('stop').classList.remove('hidden');await getParticipants();fetchLocations();
             });
 }
 //-------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
@@ -66,6 +70,38 @@ let pod_url;
 const map= L.map('map');
 const marker=L.marker([0,0]);
 //-------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+async function getIssuerFromWebID(webid){
+  var myEngine_getIssuer = new QueryEngine();
+  const bindingsStream = await myEngine_getIssuer.queryBindings(`
+  SELECT ?o WHERE {
+   ?s <http://www.w3.org/ns/solid/terms#oidcIssuer> ?o.
+  }`, {
+    sources: [`${webid}`],
+  });
+    const bindings = await bindingsStream.toArray();
+        console.log(`${bindings[0].get('o').value}`);
+      return(bindings[0].get('o').value); 
+}
+//----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+//-------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+async function getDataFromWebID(webid){
+  var myEngine_getData = new QueryEngine();
+  const bindingsStream = await myEngine_getData.queryBindings(`
+  SELECT ?img ?familyName ?givenName WHERE {
+   ?s <http://xmlns.com/foaf/0.1/img> ?img;
+  <http://xmlns.com/foaf/0.1/familyName> ?familyName;
+  <http://xmlns.com/foaf/0.1/givenName> ?givenName.
+  }`, {
+    sources: [`${webid}`],
+  });
+    const bindings = await bindingsStream.toArray();
+        console.log(`${[bindings[0].get('givenName').value,bindings[0].get('familyName').value,bindings[0].get('img').value]}`);
+      return([bindings[0].get('givenName').value,bindings[0].get('familyName').value,bindings[0].get('img').value]); 
+}
+//----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
 document.getElementById('Issuer_SC').addEventListener('click', () => { document.getElementById('Issuer_SC_username').classList.remove('hidden');document.getElementById('ok_IssuerSC').classList.remove('hidden');toHide();});
 document.getElementById('ok_IssuerSC').addEventListener('click', () => { let Username_SolidCommunity = document.getElementById('Issuer_SC_username').value; window.sessionStorage.setItem('Username_SolidCommunity_Later',Username_SolidCommunity);
 document.getElementById('ok_IssuerSC').classList.add('hidden');document.getElementById('Issuer_SC_username').classList.add('hidden');Login('https://solidcommunity.net/');});
@@ -73,20 +109,30 @@ document.getElementById('ok_IssuerSC').classList.add('hidden');document.getEleme
 document.getElementById('Issuer_CSS').addEventListener('click', () => { document.getElementById('issuer').classList.remove('hidden'); document.getElementById('Issuer_SC').classList.add('hidden'); document.getElementById('Issuer_CSS').classList.add('hidden');document.getElementById('CSS_Login').classList.remove('hidden');});
 document.getElementById('CSS_Login').addEventListener('click', () => { let Issuer = document.getElementById('issuer').value; window.sessionStorage.setItem('Issuer_later',Issuer);console.log(Issuer); Login(Issuer)});
 
-//document.getElementById('submit').addEventListener('click', ()=>{ friend_container=document.getElementById("selectPerson").value;friend_container=friend_container+'public/YourLocationHistory/';setInterval(caller,5000);/*sendAMessage();*/});
+document.getElementById('webid_Login').addEventListener('click', async () => { let webID = document.getElementById('webid').value; console.log(webID);let oidcIssuer=await getIssuerFromWebID(webID);window.sessionStorage.setItem('webID_later',webID);window.sessionStorage.setItem('oidcIssuer_later',oidcIssuer);console.log(`This is the oidcIssuer:${oidcIssuer}`); Login(oidcIssuer)});
 
-document.getElementById('message').textContent="Log in using your provider!";
+document.getElementById('message').textContent="Log in using your webid!";
 //------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 async function myfetchFunction(url){
   return await fetch(url, {
-        method: 'GET',
-//        headers: { 'Content-Type': 'application/sparql-update','Cache-Control': 'no-cache' },
+       method: 'GET',
+       headers: { 'Content-Type': 'application/sparql-update','Cache-Control': 'no-cache' },
         });
 }
 //------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 async function settingContainer(){
-
+console.log(window.sessionStorage);
 switch(Object.keys(window.sessionStorage)[0]){
+
+    case "webID_later":
+    pod_url=window.sessionStorage.getItem('oidcIssuer_later');
+    container=pod_url+'public/YourLocationHistory/';  
+    break;
+
+  case "oidcIssuer_later":
+    pod_url=window.sessionStorage.getItem('oidcIssuer_later');
+    container=pod_url+'public/YourLocationHistory/';  
+    break;
 
   case "Username_SolidCommunity_Later":
     pod_url=`https://${window.sessionStorage.getItem('Username_SolidCommunity_Later')}.solidcommunity.net/`;
@@ -103,11 +149,12 @@ switch(Object.keys(window.sessionStorage)[0]){
     }
 }  
 }
-const folder=`https://svasired.pod.knows.idlab.ugent.be/public/participants/names.ttl`;
+// const folder=`https://svasired.pod.knows.idlab.ugent.be/public/participants/names.ttl`;
+const folder=`https://sindhuvasireddy.solidcommunity.net/public/participants/names.ttl`;
 //---------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 async function storeParticipants(){
   //Storing all participant pod urls in my solid comunity pod.
-  const query= `INSERT DATA {<> <http://xmlns.com/foaf/0.1/openid> <${pod_url}>}`;
+  const query= `INSERT DATA {<> <http://xmlns.com/foaf/0.1/openid> <${window.sessionStorage.getItem('webID_later')}>}`;
 
   // Send a PATCH request the pod url to names.ttl 
   const response = await solidfetch(folder, {
@@ -140,7 +187,7 @@ async function fetchLocations(){
     window.setInterval(
       () => {
         console.log(element);
-        getLatLongofFriend(element + 'public/YourLocationHistory/');
+        getLatLongofFriend(element);
       }, 5000);
   });  
 }
@@ -162,22 +209,21 @@ async function GetCoordinates(){
         document.getElementById('map-link').appendChild(document.createElement("br"));
 
 
-        map.setView([position.coords.latitude,position.coords.longitude], 12);
+        map.setView([position.coords.latitude,position.coords.longitude], 13);
         const attribution= '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors';
         const tileURL='https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png';
 
         const tiles=L.tileLayer(tileURL,{attribution});
         tiles.addTo(map);
 
-        
-        marker.addTo(map).setLatLng([position.coords.latitude,position.coords.longitude]);
+        marker.setLatLng([position.coords.latitude,position.coords.longitude]);
 
 
         const query = `<> <https://schema.org/latitude> "${position.coords.latitude}";<https://schema.org/longitude> "${position.coords.longitude}";<http://purl.org/dc/terms/created> "${position.timestamp}".`
 
-        // Send a POST request to post to the source
-        const response = await solidfetch(container, {
-        method: 'POST',
+        // Send a PUT request to post to the source
+        const response = await solidfetch(container+`${position.timestamp}`, {
+        method: 'PUT',
         headers: { 'Content-Type': 'text/turtle' },
         body: query,
         credentials: 'include'
@@ -195,52 +241,47 @@ document.getElementById('start').classList.remove('hidden');document.getElementB
 document.querySelector('#start').addEventListener('click', () => {GetCoordinates();document.getElementById('stop').classList.remove('hidden');document.getElementById('start').classList.add('hidden');});
 //---------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 async function test(friend_container){
+      console.log(friend_container);
       var myEngine = new QueryEngine();
       // Fetch the latest timestamp  
       const bindingsStream = await myEngine.queryBindings(`
-      SELECT (MAX(?o) AS ?o) WHERE {
-        ?s <http://purl.org/dc/terms/modified> ?o.
-          }`, {
+      SELECT (STRAFTER(?fileName, "/YourLocationHistory/") AS ?timestamp) 
+      WHERE {
+        ?s <http://www.w3.org/ns/ldp#contains> ?name .
+        BIND (STR(?name) AS ?fileName)
+      }
+      ORDER BY DESC(?timestamp)`, {
         sources: [`${friend_container}`],
-        fetch:myfetchFunction
+        fetch: myfetchFunction
       });
+      myEngine.invalidateHttpCache();
+
       // Consume results as an array (easier)
       const bindings = await bindingsStream.toArray();
-
-      //------------------------------------------------------------------      
-      // Fetch the file name corresponding to the latest timestamp
-      const bindingsStream_ = await myEngine.queryBindings(`
-      SELECT ?s WHERE{
-                ?s <http://purl.org/dc/terms/modified> \"${bindings[0].get('o').value}\"^^<${bindings[0].get('o').datatype.value}>.
-      }`, {
-        sources: [`${friend_container}`],
-        fetch:myfetchFunction
-      });
-
-
-      const bindings_ = await bindingsStream_.toArray();
-
-      //-------------------------------------------------------------------
-      //Fetch the latitude from the file corresponding to the latest timestamp
+      console.log(bindings[0].get('timestamp').value);
+      const timestamp=bindings[0].get('timestamp').value;
+//---------------------------------------------------------------------------------------
+      //Fetch the lat-long from the file corresponding to the latest timestamp
       const bindingsStream_1 = await myEngine.queryBindings(`
       SELECT ?lat ?long WHERE {
       ?s <https://schema.org/latitude> ?lat ;
          <https://schema.org/longitude> ?long
       }`, {
-        sources: [`${bindings_[bindings_.length-1].get('s').value}`],
+        sources: [`${friend_container}${bindings[0].get('timestamp').value}`],
       });
 
+
       const bindings_1 = await bindingsStream_1.toArray();
-      if (bindings_1.length == 0 ){
-        console.error('Couldn’t find a long/lat pair at ', friend_container);
-        return null;
-      }
-      //console.log(bindings_1[0].get('lat').value, bindings_1[0].get('long').value, friend_container);
+
+      
       //Return the latest Latitude and Longitude:
       const lat_long_list=[bindings_1[0].get('lat').value,bindings_1[0].get('long').value];
-      return(lat_long_list);
+      let loc_array=[lat_long_list,timestamp]
+      console.log(loc_array);
+      return(loc_array);
 
 }
+//--------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
 const attribution= '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors';
 const tileURL='https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png';
@@ -249,15 +290,46 @@ const tiles=L.tileLayer(tileURL,{attribution});
 tiles.addTo(map);
 
 //---------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
-async function getLatLongofFriend(friendContainer){
-  const lat_long_list = await test(friendContainer);
-  if (lat_long_list) {
+async function getLatLongofFriend(friend_webid){
+  let friendContainer=await getIssuerFromWebID(friend_webid) + 'public/YourLocationHistory/';
+  let data_array=await getDataFromWebID(friend_webid);
+  const loc_array = await test(friendContainer);
+  const lat_long_list = loc_array[0];
+  const timestamp =loc_array[1];
+  console.log("Coming in again!");
+    if (lat_long_list) {
+      console.log(`This is the Lat-Long inside if condition:${lat_long_list}`)
     //not sure we need to center here -- need to center acording to all friend makers
-    //map.setView(lat_long_list, 15);
-    let friendMarker = L.marker(lat_long_list);
+    // map.setView(lat_long_list, 15);
+    map.eachLayer(function (layer) { 
+      console.log(layer);
+      if(layer._content){
+         if(layer._content.split('\r\n')[0]==data_array[0]+` ${data_array[1]}`){
+        console.log(`removing _tooltip: ${layer}`);
+        map.removeLayer(layer);
+              }  
+      }
+
+      if(layer._tooltipHandlersAdded){
+        console.log(layer._tooltip._content);
+        if(layer._tooltip._content.split('\r\n')[0]==data_array[0]+` ${data_array[1]}`){
+          console.log(`removing marker: ${layer}`);
+          map.removeLayer(layer);
+        }
+              }
+      }
+      );
+    var friend_image = L.icon({
+    iconUrl: data_array[2],
+    iconSize:     [30, 30], // size of the icon
+    iconAnchor:   [15, 15] // point of the icon which will correspond to marker's location
+});
+    let friendMarker = L.marker(lat_long_list,{icon: friend_image});
+    // friendMarker.addTo(map);
     friendMarker.addTo(map);
     friendMarker._icon.classList.add("huechange");
-    friendMarker.bindTooltip(friendContainer).openTooltip();
+    friendMarker.bindTooltip(data_array[0]+` ${data_array[1]}`+`\r\n Last seen at ${Date(timestamp)}`).openTooltip();
+    console.log(friendMarker.getTooltip()._content);
   }
 }
 
@@ -271,7 +343,8 @@ async function GivePublicAccesstotheContainer(){
     <http://www.w3.org/ns/auth/acl#accessTo> <${container}>;
     <http://www.w3.org/ns/auth/acl#default> <${container}>;
     <http://www.w3.org/ns/auth/acl#agentClass> <http://xmlns.com/foaf/0.1/Agent>.`
-
+    console.log(container);
+    console.log(query);
         // Send a PUT request to post to the source
         const response = await solidfetch(container+'.acl', {
         method: 'PUT',
@@ -315,4 +388,5 @@ async function GivePublicAccesstotheContainer(){
 
 
 
-
+//https://pod.rubendedecker.be/
+//https://pmcolpae.pod.knows.idlab.ugent.be/
